d97ab18d920d42ad4b1e65aae1256360ef250c49
thyeon1212 <thyeon97@naver.com>
2021-03-09 22:26:44 +0900

    주석3줄되어야함

--- Sleep_Project/app/src/main/java/com/example/sleep_project/UI_1_1_Fragement1.java
+++ Sleep_Project/app/src/main/java/com/example/sleep_project/UI_1_1_Fragement1.java
@@ -1,5 +1,6 @@
 package com.example.sleep_project;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,3 +21,4 @@ public class UI_1_1_Fragement1 extends Fragment {
 
 // good 12
 //han 12
+//han 123
