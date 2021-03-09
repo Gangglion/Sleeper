120d69302329466114be4c6008026111ff554e6b
oqtmdguqpo <o0tmdguq0o@gmail.com>
2021-03-09 20:02:40 +0900

    test

---
+++ Sleep_Project/app/src/main/java/com/example/sleep_project/UI_1_1_Fragement4.java
@@ -0,0 +1,20 @@
+package com.example.sleep_project;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class UI_1_1_Fragement4 extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            ViewGroup rootView = (ViewGroup) inflater.inflate(
+                    R.layout.ui_1_1_fragment4, container, false);
+
+            return rootView;
+        }
+}
+
