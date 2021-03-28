c0009284e02ff19cf5ddb521dbec6a852e0df4e3
oqtmdguqpo <o0tmdguq0o@gmail.com>
2021-03-28 20:04:41 +0900

    설정부분 작업중 - 설정한 값이 저장되는것 까지 확인

--- Sleep_Project/app/src/main/java/com/example/sleep_project/UI_6_SettingsFragment.java
+++ Sleep_Project/app/src/main/java/com/example/sleep_project/UI_6_SettingsFragment.java
@@ -4,6 +4,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import androidx.preference.ListPreference;
 import androidx.preference.PreferenceFragment;
@@ -15,25 +16,39 @@ import java.util.Map;
 
 public class UI_6_SettingsFragment extends PreferenceFragment {
     SharedPreferences prefs;
-    ListPreference soundPreference; //알림음 종류 설정
+    ListPreference alertreference; //알림음 종류 설정
+    //SettingSave settingsave = new SettingSave();
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        setPreferencesFromResource(R.xml.ui_6_setting_preference, rootKey);
-        soundPreference = (ListPreference) findPreference(("alert_list"));
-
+        addPreferencesFromResource(R.xml.ui_6_setting_preference);
         prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
-        prefs.registerOnSharedPreferenceChangeListener(new SharedPreferences.OnSharedPreferenceChangeListener() {
-            @Override
-            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-                Log.d("tmdguq", prefs.getAll().toString()); //설정을 변경하면 자동으로 sharedpreference에 "xml에지정한키 : 해당값" 꼴로 저장됨
-                if(prefs.getBoolean("morningQ",true)==true){
-                    //보류
-                }
+        String alert_value=prefs.getString("alert_list","");
+        //Log.d("alert_value",alert_value);
+    }
+    public void onResume() {
+        super.onResume();
+        //설정값 변경리스너..등록
+        prefs.registerOnSharedPreferenceChangeListener(listener);
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        prefs.unregisterOnSharedPreferenceChangeListener(listener);
+    }
+
+    SharedPreferences.OnSharedPreferenceChangeListener listener= new SharedPreferences.OnSharedPreferenceChangeListener() {
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            if(key.equals("morningQ")){
+                boolean Q_value= prefs.getBoolean("morningQ", false);
+                //Toast.makeText(getActivity(), "문제유무 : "+ Q_value, Toast.LENGTH_SHORT).show();
+            }else if(key.equals("alert_list")){
+                String alert_value=prefs.getString("alert_list","소리");
+                //TODO : 선택한 값에따라 Noti 방식 설정 - 다른 클래스에서 이 값들을 불러오거나, 타 클래스에 저장된 값을 불러오는 작업이 필요
+                //Toast.makeText(getActivity(), "알림방법 설정 : "+ alert_value, Toast.LENGTH_SHORT).show();
             }
-        });
-    }
-    public String getSharedP(){
-        return prefs.getAll().toString();
-    }
+        }
+    };
+
 }
