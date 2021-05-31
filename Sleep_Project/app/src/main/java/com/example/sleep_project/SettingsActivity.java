67c5295613f1188b2d4d3cac2c8c476ada5b69cb
oqtmdguqpo <o0tmdguq0o@gmail.com>
2021-06-01 07:25:31 +0900

    설정창에서 설정한대로 적용되게 함. 메인에서 설정 완료시 상단알림 띄워줌

--- Sleep_Project/app/src/main/java/com/example/sleep_project/SettingsActivity.java
+++ Sleep_Project/app/src/main/java/com/example/sleep_project/SettingsActivity.java
@@ -1,13 +1,17 @@
 package com.example.sleep_project;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
 
 public class SettingsActivity extends AppCompatActivity {
-
+    public static Context context_pref;
+    public static SharedPreferences pref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,12 +26,15 @@ public class SettingsActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+            pref = PreferenceManager.getDefaultSharedPreferences(getActivity()); //이 클래스에서 저장한 pref를 SharedPreference 객체 형태로 저장
+            context_pref = this.getContext(); //다른 클래스에서 pref를 불러오기 위함
         }
     }
 }
